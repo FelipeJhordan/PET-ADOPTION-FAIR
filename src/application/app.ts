782f5203630c0bcd5ed77ae598986b8b84ccc08b
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-27 07:57:46 -0300

    feat: add global pipes in main file

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from 'src/infra/rest/validation.pipe';
 import { AppModule } from './ioc/app.module';
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
