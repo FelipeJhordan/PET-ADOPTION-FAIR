7da80f277282b579111e9ba8b12984e9c84d1c6f
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-27 13:58:11 -0300

    feat: add creation for create table pets

---
+++ src/infra/database/migrations/1643300798624-CreatePet.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreatePet1643300798624 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'pets',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'breed',
+            type: 'varchar',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'observation',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'situation',
+            type: 'varchar',
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('pets');
+  }
+}
