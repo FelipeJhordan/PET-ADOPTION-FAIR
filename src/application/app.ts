07df4918770867cd1fc25217825f4a92b5b33f18
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-28 07:47:47 -0300

    refactor: move middlewares for infra layer

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from 'src/infra/rest/validation.pipe';
+import { ValidationPipe } from 'src/infra/rest/validation/validation.pipe';
 import { AppModule } from './ioc/app.module';
-import { applyMiddlewares } from './middlewares';
+import { applyMiddlewares } from '../infra/rest/middlewares';
+import { LoggingInterceptor } from 'src/infra/rest/intercerptors/logging.interceptor';
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
   await applyMiddlewares(app);
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
