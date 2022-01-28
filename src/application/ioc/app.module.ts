ce587afd609751201fc2a193b28d11583434beb7
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-28 09:21:26 -0300

    feat: add cache service in global app

--- src/application/ioc/app.module.ts
+++ src/application/ioc/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import {
+  CacheInterceptor,
+  CacheModule,
+  Module,
+} from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheService } from 'src/infra/cache/cache-service';
 import { PetModule } from './pet.module';
 
 @Module({
-  imports: [PetModule, TypeOrmModule.forRoot({})],
+  imports: [
+    PetModule,
+    TypeOrmModule.forRoot({}),
+    CacheModule.registerAsync({
+      useClass: CacheService,
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
