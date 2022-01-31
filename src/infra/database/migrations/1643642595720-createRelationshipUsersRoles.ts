94c0df06fa8514f808209b32a8170c2ae59a9df2
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-31 13:33:13 -0300

    chore: create migration for generate foreign key role in table users

---
+++ src/infra/database/migrations/1643642595720-createRelationshipUsersRoles.ts
@@ -0,0 +1,38 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  QueryRunnerAlreadyReleasedError,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class createRelationshipUsersRoles1643642595720
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner
+      .addColumn(
+        'users',
+        new TableColumn({
+          name: 'role',
+          type: 'int',
+        }),
+      )
+      .then(() => {
+        queryRunner.createForeignKey(
+          'users',
+          new TableForeignKey({
+            columnNames: ['role'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'roles',
+            name: 'fk_users_roles',
+          }),
+        );
+      });
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('roles', 'fk_users_roles');
+    await queryRunner.dropColumn('roles', 'role');
+  }
+}
