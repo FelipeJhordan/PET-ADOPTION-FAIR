3573cdacbf738d3812d5a1b1bae3594a917986dd
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-31 09:16:18 -0300

    feat: create integration test, with method of list and create new pet

--- test/e2e/app.e2e-spec.ts
+++ test/e2e/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../../src/application/ioc/app.module';
 import { PetModule } from 'application/ioc/pet.module';
@@ -7,6 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { setEnvironment } from 'infra/environments';
 import { ConfigModule } from '@nestjs/config';
 import { PetRepository } from 'infra/database/pets/repositories/pet.repository';
+import { mockAddPetRequestDTO } from '../units/mocks/pet.mock';
 
 describe(' (e2e)', () => {
   let app: INestApplication;
@@ -32,9 +33,39 @@ describe(' (e2e)', () => {
     petRepository = module.get<PetRepository>(PetRepository);
   });
 
+  it('/pets (POST)', async () => {
+    const { body } = await request(app.getHttpServer())
+      .post('/pets')
+      .send(mockAddPetRequestDTO)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(HttpStatus.CREATED);
+
+    console.log(body);
+    expect(body.state).toBeTruthy();
+    expect(body.breed).toBe(mockAddPetRequestDTO.breed);
+  });
   it('/pets (GET)', async () => {
-    return await request(app.getHttpServer())
+    const { body } = await request(app.getHttpServer())
       .get('/pets')
-      .expect(200);
+      .expect(HttpStatus.OK);
+
+    expect(body).toHaveLength(1);
+    expect(body[0].createdAt).toBeTruthy();
+    expect(body[0].updatedAt).toBeTruthy();
+
+    expect(body).toEqual([
+      {
+        ...mockAddPetRequestDTO,
+        createdAt: body[0].createdAt,
+        updatedAt: body[0].updatedAt,
+        deletedAt: body[0].deletedAt,
+      },
+    ]);
+  });
+
+  afterAll(async () => {
+    await petRepository.query('DELETE FROM pets;');
+    await app.close();
   });
 });
