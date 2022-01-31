09cfb34bdfd4a54b9e29ea61cf4eee76fcf89128
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-31 17:01:27 -0300

    feat: ensure if userRepository receive this correct email and ensure if findByEmail is called

--- src/application/services/user.service.ts
+++ src/application/services/user.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Person } from 'domain/models/person';
 import { User } from 'domain/models/user';
 import { IRegisterParams } from 'domain/protocols/users/register-params';
 import { IUserUseCases } from 'domain/usecases/users/pets-usecases';
+import { UserRepository } from 'infra/database/users/repositories/user.repository';
 
 @Injectable()
 export class UserService implements IUserUseCases {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {}
+
   async register(user: IRegisterParams): Promise<User> {
+    await this.userRepository.findByEmail(user.email);
+
     return new User(
       user.name,
       user.password,
--- src/infra/database/users/repositories/user.repository.ts
+++ src/infra/database/users/repositories/user.repository.ts
@@ -3,4 +3,16 @@ import { EntityRepository, Repository } from 'typeorm';
 import { UserEntity } from '../entities/user.entity';
 
 @EntityRepository(UserEntity)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async findByEmail(email: string): Promise<boolean> {
+    const emailExists = await this.findOne({
+      where: {
+        person: {
+          email: email,
+        },
+      },
+    });
+    console.log(emailExists);
+    return !!emailExists;
+  }
+}
