4d14e2fc23ee9a72e928b009215f0bdfb28f24e0
Felipe Alves <felipejordan.alves@gmail.com>
2022-02-01 14:09:18 -0300

    test: ensure that exists validation to RegisterRequestDto

--- test/units/presentation/user-controller.unit.spec.ts
+++ test/units/presentation/user-controller.unit.spec.ts
@@ -1,10 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { UserService } from 'application/services/user.service';
 import { UserController } from 'presentation/controllers/user.controller';
+import RegisterRequestDto from 'shared/dtos/user/RegisterRequestDto';
+import { validate } from 'class-validator';
 import {
   mockUser,
   mockUserRegisterRequestDto,
 } from '../mocks/user.mock';
+import { plainToInstance } from 'class-transformer';
 
 describe('<User Controller>', () => {
   let userController: UserController;
@@ -33,6 +36,26 @@ describe('<User Controller>', () => {
       await userController.register(mockUserRegisterRequestDto);
 
       expect(registerSpy).toBeCalled();
+    });
+
+    // maybe it would be better if to be in a test directory for infra layer, but ok!
+    it('Should validate params with valid pipe|class validator', async () => {
+      const bodyRequest = plainToInstance(
+        RegisterRequestDto,
+        mockUserRegisterRequestDto,
+      );
+
+      const errors = await validate(bodyRequest);
+      expect(errors.length).toBe(0);
+    });
+    it('Should validate params with valid pipe|class validator with incorrect values', async () => {
+      const bodyRequest = plainToInstance(RegisterRequestDto, {
+        ...mockUserRegisterRequestDto,
+        email: 'felipejhordan',
+      });
+
+      const errors = await validate(bodyRequest);
+      expect(errors.length).toBe(1);
     });
   });
 });
