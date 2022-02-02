8a31e4bf5475ff4738723918dde8736692c69e8b
Felipe Alves <felipejordan.alves@gmail.com>
2022-02-02 11:55:22 -0300

    test: add new tests for jwt-adapter

--- test/units/infra/jwt/jwt.unit.spec.ts
+++ test/units/infra/jwt/jwt.unit.spec.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 
 jest.mock('jsonwebtoken', () => ({
   async sign(payload: IJwtPayload): Promise<string> {
-    return String('token ');
+    return 'token';
   },
 }));
 
@@ -19,5 +19,22 @@ describe('Jwt Adapter', () => {
     expect(signSpy).toBeCalledWith({ id: '12345' }, 'test_key', {
       expiresIn: 3600,
     });
+  });
+  test('Should return a valid jwt token', async () => {
+    const sut = makeSut();
+    const signSpy = jest.spyOn(jwt, 'sign');
+    const signResult = await sut.sign({ id: '12345' });
+
+    expect(signSpy).toHaveReturnedWith(Promise.resolve('token'));
+    expect(signResult).toBe('token');
+  });
+
+  test('Should throws Error if jwtwebtoken throws', async () => {
+    const sut = makeSut();
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const promise = sut.sign({ id: '12345' });
+    await expect(promise).rejects.toThrow();
   });
 });
