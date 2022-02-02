41b78f6425b517e1e6af0e41619ff4393be2c90a
Felipe Alves <felipejordan.alves@gmail.com>
2022-02-02 14:14:53 -0300

    feat: create a custom validator contraint called CustomMatchPasswords

---
+++ src/infra/class-validator/custom-match-passwords.ts
@@ -0,0 +1,20 @@
+import {
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'CustomMatchPasswords', async: false })
+export class CustomMatchPasswords
+  implements ValidatorConstraintInterface
+{
+  validate(password: string, args: ValidationArguments) {
+    if (password !== (args.object as any)[args.constraints[0]])
+      return false;
+    return true;
+  }
+
+  defaultMessage(validationArguments?: ValidationArguments): string {
+    return 'Passwords do not match!';
+  }
+}
