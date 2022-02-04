ebcb5284554692e789cf3abccd940073a583cf2f
Felipe Alves <felipejordan.alves@gmail.com>
2022-02-04 13:22:52 -0300

    chore: create migration for add column/fk in pets table with pk of users

---
+++ src/infra/database/migrations/1643991346771-createRelationshipPetsUsers.ts
@@ -0,0 +1,38 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class createRelationshipPetsUsers1643991346771
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner
+      .addColumn(
+        'pets',
+        new TableColumn({
+          name: 'id_user',
+          type: 'uuid',
+          isNullable: true,
+        }),
+      )
+      .then(() => {
+        queryRunner.createForeignKey(
+          'pets',
+          new TableForeignKey({
+            columnNames: ['id_user'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'users',
+            name: 'fk_pets_users',
+          }),
+        );
+      });
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('pets', 'fk_pets_user');
+    await queryRunner.dropColumn('pets', 'id_user');
+  }
+}
