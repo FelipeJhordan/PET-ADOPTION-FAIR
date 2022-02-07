fc47b54cc184b1b58ce511982486ecd326c1bbae
Felipe Alves <felipejordan.alves@gmail.com>
2022-02-07 08:21:32 -0300

    feat: configure and inicializate the swagger in application

--- src/application/app.ts
+++ src/application/app.ts
@@ -4,6 +4,8 @@ import { AppModule } from './ioc/app.module';
 import { applyMiddlewares } from '../infra/rest/middlewares';
 import { LoggingInterceptor } from 'infra/rest/intercerptors/logging.interceptor';
 import { HttpExceptionFilter } from 'infra/rest/intercerptors/http-exception.filter';
+import { SwaggerModule } from '@nestjs/swagger';
+import { config } from 'infra/rest/swagger/swagger-config';
 
 declare const module: any;
 
@@ -15,6 +17,10 @@ export async function bootstrap() {
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 
   if (module.hot) {
---
+++ src/infra/rest/swagger/swagger-config.ts
@@ -0,0 +1,8 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const config = new DocumentBuilder()
+  .setTitle('Feira de adoção de animais de estimação')
+  .setDescription('The cats Api description')
+  .setVersion('1.0')
+  .addTag('pets')
+  .build(); // builder hehe
